c25da99f58a50d4744a47e4c053facd5432f2b0e
jensr <jensrodin16@gmail.com>
2025-01-24 14:57:58 +0100

    Nu blev det lite bättre

--- app/src/main/java/se/miun/dt170g/labb1/MainActivity.java
+++ app/src/main/java/se/miun/dt170g/labb1/MainActivity.java
@@ -5,8 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 /**
- * Main Activity class.
- *
+ * Main Activity class. *
  */
 public class MainActivity extends AppCompatActivity {
     /**
